1a715d257cb92a0235d03234a023d39e6c1c04ba
Sven Van Caekenberghe <sven@stfx.eu>
2026-04-13 09:51:09 +0200

    initial version of reddit live

---
+++ lib/reddit_ex_web/live/reddit_live/index.ex
@@ -0,0 +1,80 @@
+defmodule RedditWeb.RedditLive.Index do
+  use RedditWeb, :live_view
+
+  alias Reddit.Links
+  alias Reddit.Public
+
+  @impl true
+  def render(assigns) do
+    ~H"""
+    <Layouts.app flash={@flash} current_scope={@current_scope}>
+      <.header>
+        <a href={~p"/reddit"}>Reddit_ex</a>
+        <:actions :if={@current_scope}>
+          <.button variant="primary" navigate={~p"/links/new"}>
+            <.icon name="hero-plus" /> New Link
+          </.button>
+        </:actions>
+      </.header>
+
+      <.table
+        id="links"
+        rows={@streams.links}
+      >
+        <:col :let={{_id, link}} label="URL"><a href={link.url}>{link.url}</a></:col>
+        <:col :let={{_id, link}} label="Title">{link.title}</:col>
+        <:col :let={{_id, link}} label="Date">{Date.to_iso8601(link.updated_at)}</:col>
+        <:col :let={{_id, link}} label="Points">{link.points}</:col>
+        <:action :if={@current_scope}>
+          <.link>Up</.link>
+        </:action>
+        <:action :if={@current_scope}>
+          <.link>Down</.link>
+        </:action>
+        <:action :let={{_id, link}} :if={@current_scope}>
+          <.link navigate={~p"/links/#{link}/edit"}>Edit</.link>
+        </:action>
+        <:action :let={{id, link}} :if={@current_scope}>
+          <.link
+            phx-click={JS.push("delete", value: %{id: link.id}) |> hide("##{id}")}
+            data-confirm="Are you sure?"
+          >
+            Delete
+          </.link>
+        </:action>
+      </.table>
+    </Layouts.app>
+    """
+  end
+
+  @impl true
+  def mount(_params, _session, socket) do
+    if connected?(socket) && socket.assigns.current_scope do
+      # fix: even public needs global scoped updates
+      Links.subscribe_links(socket.assigns.current_scope)
+    end
+
+    {:ok,
+     socket
+     |> assign(:page_title, "Reddit_ex")
+     |> stream(:links, list_links())}
+  end
+
+  @impl true
+  def handle_event("delete", %{"id" => id}, socket) do
+    link = Links.get_link!(socket.assigns.current_scope, id)
+    {:ok, _} = Links.delete_link(socket.assigns.current_scope, link)
+
+    {:noreply, stream_delete(socket, :links, link)}
+  end
+
+  @impl true
+  def handle_info({type, %Reddit.Links.Link{}}, socket)
+      when type in [:created, :updated, :deleted] do
+    {:noreply, stream(socket, :links, list_links(), reset: true)}
+  end
+
+  defp list_links() do
+    Public.list_links_highest_points(5)
+  end
+end
--- lib/reddit_ex_web/router.ex
+++ lib/reddit_ex_web/router.ex
@@ -72,6 +72,7 @@ defmodule RedditWeb.Router do
 
     live_session :current_user,
       on_mount: [{RedditWeb.UserAuth, :mount_current_scope}] do
+      live "/reddit", RedditLive.Index, :index
       live "/users/register", UserLive.Registration, :new
       live "/users/log-in", UserLive.Login, :new
       live "/users/log-in/:token", UserLive.Confirmation, :new
