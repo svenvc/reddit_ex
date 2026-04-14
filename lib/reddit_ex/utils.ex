63361fef69a4dd9c5150e5e7bdb2071859e646bf
Sven Van Caekenberghe <sven@stfx.eu>
2026-04-14 15:21:47 +0200

    improve url validation

--- lib/reddit_ex/utils.ex
+++ lib/reddit_ex/utils.ex
@@ -1,24 +1,44 @@
 defmodule Reddit.Utils do
   def validate_url_and_extract_html_title(url) when is_binary(url) do
-    case Req.get(url) do
-      {:ok,
-       %Req.Response{
-         status: 200,
-         headers: %{"content-type" => ["text/html" <> _more]},
-         body: body
-       }} ->
-        body
-        |> Floki.parse_document!()
-        |> Floki.find("title")
-        |> List.first()
-        |> Floki.text()
-        |> String.trim()
-        |> String.split()
-        |> Enum.join(" ")
-        |> then(&{:ok, &1})
+    case validate_url(url) do
+      {:ok, url} ->
+        case Req.get(url) do
+          {:ok,
+           %Req.Response{
+             status: 200,
+             headers: %{"content-type" => ["text/html" <> _more]},
+             body: body
+           }} ->
+            body
+            |> Floki.parse_document!()
+            |> Floki.find("title")
+            |> List.first()
+            |> Floki.text()
+            |> String.trim()
+            |> String.split()
+            |> Enum.join(" ")
+            |> then(&{:ok, &1})
 
-      _ ->
-        {:error, "failed to resolve url and find html title"}
+          _ ->
+            {:error, "failed to resolve url and find html title"}
+        end
+
+      {:error, msg} ->
+        {:error, msg}
+    end
+  end
+
+  def validate_url(url) when is_binary(url) do
+    case URI.new(url) do
+      {:ok, uri} ->
+        if uri.scheme in ~w(http https) && uri.host != "" do
+          {:ok, uri}
+        else
+          {:error, "malformed/unsupported url"}
+        end
+
+      {:error, _error} ->
+        {:error, "malformed url"}
     end
   end
 end
