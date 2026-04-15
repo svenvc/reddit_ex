beb8d50cbb69e7c4f845c609429bcfa8f488cf20
Sven Van Caekenberghe <sven@stfx.eu>
2026-04-15 10:56:32 +0200

    fix test tracking text change

--- test/reddit_ex_web/controllers/page_controller_test.exs
+++ test/reddit_ex_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule RedditWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Aggregates links, submitted and voted by users"
+    assert html_response(conn, 200) =~ "Aggregated links, submitted and voted by users"
   end
 end
